c1f65109d148350fc92a07b81dac9f49ee40c3ef
arfurlaneto <arfurlaneto@gmail.com>
2021-11-18 22:00:10 -0300

    feat: generating random names, showing a list of names

--- node/index.js
+++ node/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mysql = require('mysql')
+const random_name = require('node-random-name');
 
 const port = 3000
 const config = {
@@ -12,16 +13,17 @@ const config = {
 // Migrations
 const connection = mysql.createConnection(config)
 connection.query('create table if not exists people(id int not null auto_increment, name varchar(255), primary key(id));'); 
-connection.query("insert into people(name) values('Tony');")
+connection.query(`insert into people(name) values('${random_name()}');`)
 connection.end();
 
 const app = express()
 
 app.get('/', (req, res) => {
   const connection = mysql.createConnection(config)
-  connection.query("select name from people limit 1;", function(error, results) {
+  connection.query("select name from people;", function(error, results) {
     if (error) throw error;
-    res.send(`<h1>Full Cycle! Name is ${results[0].name}!</h1>`)
+    res.send(`<h1>Full Cycle Rocks!</h1>`
+    +`<ul>${results.map(result => `<li>${result.name}</li>`).join('')}</ul>`)
   })
   connection.end();
 })
